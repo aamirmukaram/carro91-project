10c042f24b27ec060a182043563eebb09414954b
Aamir Mukaram <aamirmukaram@gmail.com>
2016-09-15 02:29:03 +0500

    add chart.js cdn

--- app/frontend/assets/js/config.constant.js
+++ app/frontend/assets/js/config.constant.js
@@ -16,7 +16,7 @@ app.constant('JS_REQUIRES', {
         'd3': '../../bower_components/d3/d3.min.js',
 
         //*** jQuery Plugins
-        'chartjs': '../../bower_components/Chart.js/dist/Chart.min.js',
+        'chartjs': 'https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.2.2/Chart.min.js',
         'ckeditor-plugin': '../../bower_components/ckeditor/ckeditor.js',
         'jquery-nestable-plugin': ['../../bower_components/jquery-nestable/jquery.nestable.js'],
         'touchspin-plugin': ['../../bower_components/bootstrap-touchspin/dist/jquery.bootstrap-touchspin.min.js', '../../bower_components/bootstrap-touchspin/dist/jquery.bootstrap-touchspin.min.css'],
